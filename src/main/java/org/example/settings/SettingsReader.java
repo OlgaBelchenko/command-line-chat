3a12bd6f9f967753c0f6d70c84ef4eae0f63e467
Olga Belchenko <>
2024-01-12 14:49:53 +0500

    Settings reading, writing refactored

--- src/main/java/org/example/settings/SettingsReader.java
+++ src/main/java/org/example/settings/SettingsReader.java
@@ -1,16 +1,39 @@
 package org.example.settings;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
 public class SettingsReader {
 
+    private final String SETTINGS_FILE_PATH = "src/main/resources/settings.txt";
+    private final Map<String, String> settings = new HashMap<>();
+
+    public SettingsReader() {
+        fillSettingsMap();
+    }
+
     public int getPort() {
-        Map<String, String> settings = SettingsWriter.getSettings();
         return Integer.parseInt(settings.getOrDefault("port", "-1"));
     }
 
     public String getHost() {
-        Map<String, String> settings = SettingsWriter.getSettings();
         return settings.getOrDefault("host", "");
+    }
+
+    private void fillSettingsMap() {
+        String line;
+        try (BufferedReader reader = new BufferedReader(new FileReader(SETTINGS_FILE_PATH))) {
+            while ((line = reader.readLine()) != null) {
+                if (line.length() > 1) {
+                    String[] setting = line.split(":", 2);
+                    settings.put(setting[0], setting[1]);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/org/example/settings/SettingsWriter.java
+++ src/main/java/org/example/settings/SettingsWriter.java
@@ -1,38 +1,15 @@
 package org.example.settings;
 
 import java.io.*;
-import java.util.HashMap;
-import java.util.Map;
 
 public class SettingsWriter {
-    private static final String SETTINGS_FILE_PATH = "src/main/resources/settings.txt";
-    private static final Map<String, String> settings = new HashMap<>();
+    private final String SETTINGS_FILE_PATH = "src/main/resources/settings.txt";
 
     public void writeSettingsToFile(String settings) {
         try (PrintWriter writer = new PrintWriter(new FileWriter(SETTINGS_FILE_PATH, false))) {
             writer.write(settings);
-            fillSettingsMap();
         } catch (IOException e) {
             throw new RuntimeException(e);
-        }
-    }
-
-    public static Map<String, String> getSettings() {
-        fillSettingsMap();
-        return settings;
-    }
-
-    public static void fillSettingsMap() {
-        String line;
-        try (BufferedReader reader = new BufferedReader(new FileReader(SETTINGS_FILE_PATH))) {
-            while ((line = reader.readLine()) != null) {
-                if (line.length() > 1) {
-                    String[] setting = line.split(":", 2);
-                    settings.put(setting[0], setting[1]);
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 }
