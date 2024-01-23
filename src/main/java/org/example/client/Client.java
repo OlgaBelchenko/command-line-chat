8e722c82f4aed3d5f97bbeb4b0b3e6b163e5e83f
Olga Belchenko <>
2024-01-23 09:53:55 +0500

    Client refactored

--- src/main/java/org/example/client/Client.java
+++ src/main/java/org/example/client/Client.java
@@ -5,6 +5,8 @@ import org.example.settings.SettingsReader;
 
 import java.io.*;
 import java.net.Socket;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 import java.util.Scanner;
 
 import static java.lang.System.exit;
@@ -16,17 +18,28 @@ public class Client {
     private BufferedWriter out;
     private String userName;
     private final String EXIT_COMMAND = "/exit";
-    private static final Logger logger = Logger.getInstance();
-    private static final String LOG_FILE_PATH = "src/main/resources/client_log.txt";
-    private static final String SETTINGS_FILE_PATH = "src/main/resources/settings.txt";
-    private static final String PORT = "port";
-    private static final String HOST = "host";
+    private final Logger logger = Logger.getInstance();
+    private final String LOG_FILE_PATH = "src/main/resources/client_log.txt";
+    private final String SETTINGS_FILE_PATH = "src/main/resources/settings.txt";
+    private final String PORT_PARAMETER = "port";
+    private final String HOST_PARAMETER = "host";
 
 
-    public Client(Socket socket, String userName) {
+    public Client() {
         try {
+            userName = getUserNameFromUser();
+            socket = getSocketFromFile();
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        } catch (IOException e) {
+            shutdownClient();
+        }
+    }
+
+    public Client(Socket socket) {
+        try {
+            userName = getUserNameFromUser();
             this.socket = socket;
-            this.userName = userName;
             in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
         } catch (IOException e) {
@@ -62,13 +75,19 @@ public class Client {
                 while (socket.isConnected()) {
                     messageFromServer = in.readLine();
                     if (messageFromServer == null) continue;
-                    System.out.println(messageFromServer);
+                    System.out.println(getCurrentTime() + messageFromServer);
                     logger.log(messageFromServer, LOG_FILE_PATH);
                 }
             } catch (IOException e) {
                 shutdownClient();
             }
         }).start();
+    }
+
+    private String getCurrentTime() {
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        String dateTime = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss").format(timestamp);
+        return dateTime + " ";
     }
 
     private void shutdownClient() {
@@ -82,7 +101,6 @@ public class Client {
             if (socket != null) {
                 socket.close();
             }
-
             logger.log(userName + " вышел из чата.", LOG_FILE_PATH);
             exit(0);
         } catch (IOException e) {
@@ -90,13 +108,13 @@ public class Client {
         }
     }
 
-    private static Socket getSocket() throws IOException {
+    private Socket getSocketFromFile() throws IOException {
         SettingsReader sr = new SettingsReader(SETTINGS_FILE_PATH);
-        String host = sr.getSetting(HOST);
+        String host = sr.getSetting(HOST_PARAMETER);
         if (host.isEmpty()) {
             throw new IllegalArgumentException("Параметра host нет в файле настроек!)");
         }
-        String port = sr.getSetting(PORT);
+        String port = sr.getSetting(PORT_PARAMETER);
         if ("".equals(port)) {
             throw new IllegalArgumentException("Параметра port нет в файле настроек!)");
         }
@@ -104,28 +122,30 @@ public class Client {
         return new Socket(host, Integer.parseInt(port));
     }
 
-    private static boolean isUserNameCorrect(String userName) {
+    private boolean isUserNameCorrect(String userName) {
         return userName.length() >= 2 && userName.length() <= 13;
     }
 
-    public static void main(String[] args) throws IOException {
+    public void start() {
+//        userName = getUserNameFromUser();
+        listenToMessage();
+        sendMessage();
+    }
+
+    private String getUserNameFromUser() {
         Scanner scanner = new Scanner(System.in);
         String enterYourName = "Введите свое имя:";
         System.out.println(enterYourName);
         logger.log(enterYourName, LOG_FILE_PATH);
-        String userName = scanner.nextLine();
-        logger.log(userName, LOG_FILE_PATH);
-        while (!isUserNameCorrect(userName)) {
+        String name = scanner.nextLine();
+        logger.log(name, LOG_FILE_PATH);
+        while (!isUserNameCorrect(name)) {
             String wrongName = "Некорректное имя! Имя должно быть более 2 символов, менее 13 и не быть пустым! Введите другое имя:";
             System.out.println(wrongName);
             logger.log(wrongName, LOG_FILE_PATH);
-            userName = scanner.nextLine();
-            logger.log(userName, LOG_FILE_PATH);
+            name = scanner.nextLine();
+            logger.log(name, LOG_FILE_PATH);
         }
-        Socket socket = getSocket();
-        Client client = new Client(socket, userName);
-        client.listenToMessage();
-        client.sendMessage();
+        return name;
     }
-
 }
